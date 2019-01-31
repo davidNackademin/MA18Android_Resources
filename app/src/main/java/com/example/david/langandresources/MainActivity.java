399133596acb18cec7660d9f6f2ef764861f9a02
David Svensson <david.svensson@nackademin.se>
2019-01-31 10:24:33 +0100

    Added button funktionality

--- app/src/main/java/com/example/david/langandresources/MainActivity.java
+++ app/src/main/java/com/example/david/langandresources/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.david.langandresources;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView helloView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helloView = findViewById(R.id.textView);
+
     }
+
+    public void buttonPressed(View view) {
+
+
+
+        helloView.setText(getString(R.string.good_bye));
+
+
+    }
+
+
+
 }
